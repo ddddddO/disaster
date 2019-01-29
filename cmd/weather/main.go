d71fa6816dd1e4f35b1f4a1d89e939c677dc4496
dddddO <lbfdeatq@gmail.com>
2019-01-29 23:10:57 +0900

    get request OK!

--- cmd/weather/main.go
+++ cmd/weather/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,19 +12,25 @@ import (
 	api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
 	{city name},{country code} は、以下参考
 	http://www.asahi-net.or.jp/~ax2s-kmtn/ref/iso3166-1.html
-
-	NOTE:APIkey使えるようになるまで時間かかるっぽい
 */
 
-const endpoint = "api.openweathermap.org/data/2.5/forecast"
+const endpoint = "http://api.openweathermap.org/data/2.5/forecast"
 
 func main() {
 	target := endpoint + "?q=%s,%s&APPID=%s"
 
-	resp, err := http.Get(fmt.Sprintf(target, "London", "uk", "<apiid>"))
+	resp, err := http.Get(fmt.Sprintf(target, "London", "uk", "appid"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(resp)
+	if resp.StatusCode != http.StatusOK {
+		log.Fatal(errors.New("failed to request"))
+	}
+
+	sc := bufio.NewScanner(resp.Body)
+	for sc.Scan() {
+		fmt.Println(sc.Text())
+	}
+
 }
